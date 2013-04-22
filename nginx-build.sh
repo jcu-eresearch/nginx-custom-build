ae31e3c8fb1f575bcbc1d142ccc7c4a1d90f4395
David Beitey <david@davidjb.com>
2013-04-23 08:42:38 +1000

    Fix for fancyindex was merged.  Hooray.

--- nginx-build.sh
+++ nginx-build.sh
@@ -3,7 +3,7 @@ pushd ~/rpmbuild/SOURCES
 hg clone https://bitbucket.org/davidjb/ngx_http_auth_request_module
 wget https://github.com/agentzh/headers-more-nginx-module/archive/v0.19.tar.gz -O headers-more-nginx-module-0.19.tar.gz
 tar xvf headers-more-nginx-module-0.19.tar.gz
-git clone git://github.com/davidjb/ngx-fancyindex.git
+git clone git://github.com/aperezdc/ngx-fancyindex.git
 git clone git://github.com/yaoweibin/nginx_ajp_module.git
 popd
 pushd ~/rpmbuild/SPECS
