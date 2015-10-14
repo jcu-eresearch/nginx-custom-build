cdef60d294bca258c7d48171d61b525185176ebc
David\ Beitey <david@davidjb.com>
2015-10-14 17:07:39 +1000

    Minor upgrade to modules, bump build version

--- nginx-build.sh
+++ nginx-build.sh
@@ -30,34 +30,34 @@ popd
 #Get various add-on modules for Nginx
 #XXX git clone -b [tag] isn't supported on git 1.7 (RHEL 6)
 pushd ~/rpmbuild/SOURCES
-  
+
     #Headers More module
     git clone https://github.com/openresty/headers-more-nginx-module
     pushd headers-more-nginx-module
-    git checkout v0.26
+    git checkout v0.261
     popd
-    
+
     #Fancy Index module
     git clone https://github.com/aperezdc/ngx-fancyindex.git
     pushd ngx-fancyindex
     git checkout v0.3.5
     popd
-    
+
     #AJP module
     git clone https://github.com/yaoweibin/nginx_ajp_module.git
     pushd nginx_ajp_module
     git checkout bf6cd93
     popd
-    
+
     #LDAP authentication module
     git clone https://github.com/kvspb/nginx-auth-ldap.git
     pushd nginx-auth-ldap
-    git checkout a8b5948e0e
+    git checkout 928856a
     popd
-    
+
     #Shibboleth module
     git clone https://github.com/nginx-shib/nginx-http-shibboleth.git
-  
+
 popd
 
 # Obtain a location for the patches, either from /vagrant
